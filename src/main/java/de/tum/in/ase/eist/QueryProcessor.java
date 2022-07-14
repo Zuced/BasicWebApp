589807e568a8c97e366099206d29b4f40cddc165
DESKTOP-8S1B8S4\kevin <ge47tuk@mytum.de>
2022-07-14 13:25:27 +0200

    largest

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,15 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.toLowerCase().contains("name")) {
-           return "Kevin";
+            return "Kevin";
+        } else if (query.toLowerCase().contains("largest")) {
+            String[] list = query.split("[ ,]*|,[ ,]*");
+            int max = 0;
+            for(String str : list) {
+                int number = Integer.parseInt(str);
+                max = Math.max(number, max);
+            }
+            return Integer.toString(max);
         } else { // TODO extend the programm here
             return "";
         }
